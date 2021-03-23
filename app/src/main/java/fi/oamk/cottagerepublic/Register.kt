b5813f2b7991f0e0d533795fb7c53698ba394097
Jami Hannus <t9haja00@student.oamk.fi>
2021-03-23 15:11:07 +0200

    Added a new register activity

---
+++ app/src/main/java/fi/oamk/cottagerepublic/Register.kt
@@ -0,0 +1,11 @@
+package fi.oamk.cottagerepublic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screen_register_main)
+    }
+}

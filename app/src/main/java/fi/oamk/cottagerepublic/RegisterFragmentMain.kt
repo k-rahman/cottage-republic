3c5ae1b507364fb41de7a252efcaaedfedaa728a
Jami Hannus <t9haja00@students.oamk.fi>
2021-03-27 21:11:45 +0200

    Added navigation fragment for registering

---
+++ app/src/main/java/fi/oamk/cottagerepublic/RegisterFragmentMain.kt
@@ -0,0 +1,27 @@
+package fi.oamk.cottagerepublic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+
+class RegisterFragmentMain : Fragment() {
+    lateinit var navController: NavController
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register_main, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.navigate_button_main).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_fragment_register_main_to_registerFragmentName, null))
+    }
+}
---
+++ app/src/main/java/fi/oamk/cottagerepublic/RegisterFragmentName.kt
@@ -0,0 +1,27 @@
+package fi.oamk.cottagerepublic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+
+class RegisterFragmentName : Fragment() {
+    lateinit var navController: NavController
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register_name, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.navigate_button_name).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_registerFragmentName_to_registerFragmentRequiredInfo, null))
+    }
+}

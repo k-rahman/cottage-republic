6ffd087ec6a13d5cd483a9a97746d2e9731c2834
Jami Hannus <t9haja00@students.oamk.fi>
2021-03-28 07:51:24 +0300

    v1 of register_screen.xml non used code removed

--- app/src/main/java/fi/oamk/cottagerepublic/RegisterScreen.kt
+++ app/src/main/java/fi/oamk/cottagerepublic/RegisterScreen.kt
@@ -2,10 +2,17 @@ package fi.oamk.cottagerepublic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import fi.oamk.cottagerepublic.databinding.RegisterScreenBinding
+
 
 class RegisterScreen : AppCompatActivity() {
+
+    private lateinit var binding: RegisterScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_screen)
+        binding = RegisterScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

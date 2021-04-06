5473bcef29429938d68eb739e3f494da68e63eb1
Alex-Vel <t9veal00@students.oamk.fi>
2021-04-06 13:47:11 +0300

    login screen two-way binding

--- app/src/main/java/fi/oamk/cottagerepublic/home/BindingUtils.kt
+++ app/src/main/java/fi/oamk/cottagerepublic/home/BindingUtils.kt
@@ -1,12 +1,12 @@
 package fi.oamk.cottagerepublic.home
 
-import android.widget.EditText
+
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import fi.oamk.cottagerepublic.Cottage
 import fi.oamk.cottagerepublic.Destination
-import fi.oamk.cottagerepublic.auth.LoginScreenViewModel
+
 
 @BindingAdapter("image")
 fun ImageView.setDestinationImage(item: Destination) = setImageResource(item.image)
@@ -23,10 +23,4 @@ fun ImageView.setCottageImage(item: Cottage) = setImageResource(item.image)
 @BindingAdapter("cottageLabel")
 fun TextView.setCottageName(item: Cottage) {
    text = item.cottageLabel
-}
-
-@BindingAdapter("username")
-fun EditText.writeUsername(item: LoginScreenViewModel)
-{
-   setText(item.username.value)
 }

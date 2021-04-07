ab13ce25e84c400918e156cfddfd675f1bda136f
Alex-Vel <t9veal00@students.oamk.fi>
2021-04-07 18:15:31 +0300

    Viewmodel repo reference

--- app/src/main/java/fi/oamk/cottagerepublic/auth/LoginScreenViewModel.kt
+++ app/src/main/java/fi/oamk/cottagerepublic/auth/LoginScreenViewModel.kt
@@ -3,8 +3,10 @@ package fi.oamk.cottagerepublic.auth
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import fi.oamk.cottagerepublic.repository.RepositoryModel
 
 class LoginScreenViewModel : ViewModel() {
+    lateinit var repository: RepositoryModel
     val username = MutableLiveData<String>()
     val password = MutableLiveData<String>()
     private var _navigate = MutableLiveData<Boolean>()
@@ -24,9 +26,14 @@ fun onLoginClick()
         //Call login function from repository
         //if response successful
 
+
         _navigate.value = true
 
 
+    }
+
+    fun onLoginClick(email: String?, password: String?) {
+        repository.login(email, password)
     }
 
     fun doneNavigate()

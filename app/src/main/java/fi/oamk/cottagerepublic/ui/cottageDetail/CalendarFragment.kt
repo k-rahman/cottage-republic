28bbde5eb94929f4517ca847dad4e50f53f6c80f
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-04-09 23:15:42 +0300

    Update calendar layout and add shared viewModel with cottage detail via navigation graph

--- app/src/main/java/fi/oamk/cottagerepublic/ui/cottageDetail/CalendarFragment.kt
+++ app/src/main/java/fi/oamk/cottagerepublic/ui/cottageDetail/CalendarFragment.kt
@@ -6,31 +6,36 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.savvi.rangedatepicker.CalendarPickerView
 import fi.oamk.cottagerepublic.R
 import fi.oamk.cottagerepublic.databinding.FragmentCalendarBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 class CalendarFragment : Fragment() {
     private lateinit var  binding: FragmentCalendarBinding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private lateinit var viewModel: CottageDetailViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_calendar, container, false)
+        binding.lifecycleOwner = this
 
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        val backStackEntry = findNavController().getBackStackEntry(R.id.cottageDetailFragment)
+        viewModel = ViewModelProvider(backStackEntry).get(CottageDetailViewModel::class.java)
+        binding.viewModel = viewModel
 
         val nextYear = Calendar.getInstance()
-        nextYear.add(Calendar.YEAR, 10);
+        nextYear.add(Calendar.YEAR, 10)
 
-        val lastYear = Calendar.getInstance();
-        lastYear.add(Calendar.YEAR, -10);
+        val lastYear = Calendar.getInstance()
+        lastYear.add(Calendar.YEAR, -10)
 
-        val calendar = view.findViewById<CalendarPickerView>(R.id.calendar)
-        calendar.init(lastYear.time , nextYear.time)
+        binding.calendar.init(lastYear.time , nextYear.time, SimpleDateFormat("MMMM  YYYY", Locale.getDefault()))
             .inMode(CalendarPickerView.SelectionMode.RANGE)
             .withSelectedDate(Date())
+
+        return binding.root
     }
 }

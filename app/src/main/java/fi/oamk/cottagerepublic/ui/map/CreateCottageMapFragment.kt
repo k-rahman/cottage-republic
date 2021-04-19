589c65ece90d8b7291c6c214be7e398e9be7272d
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-04-19 14:13:03 +0300

    Implement OnMapClickListener on CreateCottageMapFragment class. Implement onMapclick event handler

--- app/src/main/java/fi/oamk/cottagerepublic/ui/map/CreateCottageMapFragment.kt
+++ app/src/main/java/fi/oamk/cottagerepublic/ui/map/CreateCottageMapFragment.kt
@@ -1,21 +1,17 @@
 package fi.oamk.cottagerepublic.ui.map
 
-import android.content.res.Resources
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.mapbox.geojson.Feature
 import com.mapbox.geojson.Point
-import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
@@ -28,8 +24,7 @@ import fi.oamk.cottagerepublic.util.MapUtils
 class CreateCottageMapFragment : Fragment(), MapboxMap.OnMapClickListener {
     private lateinit var binding: FragmentMapBinding
     private lateinit var viewModel: CreateCottageViewModel
-    private var mapboxMap: MapboxMap? = null
-    val SOURCE_ID = "SOURCE_ID"
+    private lateinit var mapUtils: MapUtils
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,39 +39,41 @@ class CreateCottageMapFragment : Fragment(), MapboxMap.OnMapClickListener {
         viewModel = ViewModelProvider(backStackEntry).get(CreateCottageViewModel::class.java)
         binding.createViewModel = viewModel
 
-
         // toolbar configuration
         val appBarConfiguration = AppBarConfiguration(findNavController().graph)
         binding.toolbar.setupWithNavController(findNavController(), appBarConfiguration)
         binding.toolbar.setNavigationIcon(R.drawable.icon_back_arrow_24)
 
-
-
         // hide bottom navigation
         requireActivity().findViewById<View>(R.id.bottom_nav_view).visibility = View.GONE
 
+        mapUtils = MapUtils(
+            savedInstanceState,
+            requireContext(),
+            binding.cottageMap,
+            true,
+            this@CreateCottageMapFragment,
+        )
 
-      //  val locationsList = arrayListOf<HashMap<String, Double>>()
-        mapboxMap = MapUtils.initializeCreateCottageMap(savedInstanceState, resources, binding.cottageMap, this)
+        mapUtils.mapboxMap.observe(viewLifecycleOwner, {
+            mapUtils.initCameraPosition(hashMapOf("lat" to 65.142455, "long" to 27.078449))
+        })
 
         return binding.root
     }
 
     override fun onMapClick(point: LatLng): Boolean {
-        mapboxMap?.getStyle{
-            val geoJsonSource = it.getSourceAs<GeoJsonSource>(SOURCE_ID)
-            geoJsonSource?.setGeoJson(Point.fromLngLat(point.longitude,point.latitude))
+        mapUtils.mapboxMap.value?.getStyle {
+            // Update the SymbolLayer icon's source to move the map pin
+            val geoJsonSource = it.getSourceAs<GeoJsonSource>(MapUtils.SOURCE_ID)
+            geoJsonSource?.setGeoJson(Feature.fromGeometry(Point.fromLngLat(point.longitude, point.latitude)))
         }
-//        Toast.makeText(
-//            context,
-//            String.format("User clicked at: %s", point.toString()),
-//            Toast.LENGTH_LONG
-//        ).show()
-        Log.i("coordinates",point.toString())
 
-        viewModel.cottageCoordinates = hashMapOf("long" to point.longitude, "lat" to point.latitude)
+        viewModel.cottageCoordinates =
+            hashMapOf("long" to point.longitude, "lat" to point.latitude)
+
         return true
-        }
+    }
 }
 
 

bbc62dc71b8263e3942fa37123c32167c76d8bea
Jami Hannus <t9haja00@students.oamk.fi>
2021-04-22 10:29:15 +0300

    Removed unneeded listeners

--- app/src/main/java/fi/oamk/cottagerepublic/repository/UserRepository.kt
+++ app/src/main/java/fi/oamk/cottagerepublic/repository/UserRepository.kt
@@ -2,6 +2,7 @@ package fi.oamk.cottagerepublic.repository
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
@@ -15,63 +16,60 @@ class UserRepository {
     private lateinit var userProfile: DataSnapshot
     private var userid = firebaseAuth.currentUser
 
-    fun createUser(email: String) {
-        databaseReference.child("users").child(userid!!.uid).child("email").setValue(email).addOnSuccessListener {
-            return@addOnSuccessListener
-        }.addOnFailureListener { return@addOnFailureListener }
 
-    fun createUser(email: String) {
-        databaseReference.child("users").child(userid!!.uid).child("email").setValue(email)
-    }
+        fun getCurrentUserId(): String {
+            if (userid != null)
+                return userid?.uid.toString()
+            return "User isn't logged in"
+        }
 
-    fun getCurrentUserId(): String {
-        if (userid != null)
-            return userid?.uid.toString()
-
-        return "User isn't logged in"
-    }
-    fun getCurrentUserData(email: MutableLiveData<String>, fname: MutableLiveData<String>, lname: MutableLiveData<String>, phone: MutableLiveData<String>): Boolean {
-
-        if (firebaseAuth.currentUser != null) {
-            databaseReference.child("users").child(userid!!.uid).get().addOnSuccessListener {
-
-                if (it.child("email").value != null) email.postValue(it.child("email").value.toString())
-                if (it.child("phone").value != null) phone.postValue(it.child("phone").value.toString())
-                if (it.child("fname").value != null) fname.postValue(it.child("fname").value.toString())
-                if (it.child("lname").value != null) lname.postValue(it.child("lname").value.toString())
-
-                userProfile = it
-                Log.i("data check", userProfile.toString())
-            }.addOnFailureListener {
-                throw Exception("Failure loading data from server")
+        fun getCurrentUserData(email: MutableLiveData<String>, fname: MutableLiveData<String>, lname: MutableLiveData<String>, phone: MutableLiveData<String>): Boolean {
+            if (firebaseAuth.currentUser != null) {
+                databaseReference.child("users").child(userid!!.uid).get().addOnSuccessListener {
+                    if (it.child("email").value != null) email.postValue(it.child("email").value.toString())
+                    if (it.child("phone").value != null) phone.postValue(it.child("phone").value.toString())
+                    if (it.child("fname").value != null) fname.postValue(it.child("fname").value.toString())
+                    if (it.child("lname").value != null) lname.postValue(it.child("lname").value.toString())
+                    userProfile = it
+                    Log.i("data check", userProfile.toString())
+                }.addOnFailureListener {
+                    Log.i("failure" , "there is some problem getting the data")
+                }
+                return true
+            } else {
+                return false
             }
-            return true
-        }
-        else{
-            return false
         }
 
-    }
-    fun updateUserData(fname: String, lname: String, phone: String): Boolean {
-        if (userid !=null) {
-           if(fname != userProfile.child("fname").value.toString()){
-               databaseReference.child("users").child(userid!!.uid).child("fname").setValue(fname).addOnFailureListener { return@addOnFailureListener }
-           }
-            if(lname != userProfile.child("lname").value.toString()){
-                databaseReference.child("users").child(userid!!.uid).child("lname").setValue(lname).addOnFailureListener { return@addOnFailureListener }
+        fun updateUserData(fname: String, lname: String, phone: String): Boolean {
+            //if not dont have logged in user will return false
+            if (userid != null) {
+                if (fname != userProfile.child("fname").value.toString()) {
+                    databaseReference.child("users").child(userid!!.uid).child("fname").setValue(fname)
+                }
+                if (lname != userProfile.child("lname").value.toString()) {
+                    databaseReference.child("users").child(userid!!.uid).child("lname").setValue(lname)
+                }
+                if (phone != userProfile.child("phone").value.toString()) {
+                    databaseReference.child("users").child(userid!!.uid).child("phone").setValue(phone)
+                }
+                Log.i("data update", "data saved")
+                // if there is a logged in user to save data
+                return true
+            } else {
+                Log.i("data update", "data save failed")
+                return false
             }
-            if(phone != userProfile.child("phone").value.toString()){
-                databaseReference.child("users").child(userid!!.uid).child("phone").setValue(phone).addOnFailureListener { return@addOnFailureListener }
-            }
-            Log.i("data check", "data saved")
-            return true
         }
-        else{
-            return false
+
+        fun getUserData(): DataSnapshot {
+            return userProfile
         }
-    }
-    fun getUserData(): DataSnapshot {
-        return userProfile
+    fun getUserReservations(): Task<DataSnapshot> {
+        val testid = "6EAP6t8B8wROG6OYsPwzaHBntjE2"
+        val data = databaseReference.child("users").child(testid).child("reservations").get()
+        return data
     }
 
 }
+

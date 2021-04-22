b3a5d4fe65c43d583f132cec1fcd889b88915444
Anthony Harvey <anthony21harvey@gmail.com>
2021-04-22 13:12:31 +0300

    Passing arguments from MyCottages screen to AddCottageFormFragment screen

--- app/src/main/java/fi/oamk/cottagerepublic/ui/account/AddCottageFormFragment.kt
+++ app/src/main/java/fi/oamk/cottagerepublic/ui/account/AddCottageFormFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import fi.oamk.cottagerepublic.R
 import fi.oamk.cottagerepublic.databinding.FragmentAddCottageFormBinding
 
@@ -18,7 +20,7 @@ class AddCottageFormFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Get a reference to the binding object and inflate the fragment views.
         binding = DataBindingUtil.inflate(
             inflater,
@@ -26,6 +28,14 @@ class AddCottageFormFragment : Fragment() {
             container,
             false
         )
+        val args = AddCottageFormFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "Cottage: ${args.cottage.cottageLabel}", Toast.LENGTH_SHORT).show()
+
+
+//        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
     }
+
+
 }

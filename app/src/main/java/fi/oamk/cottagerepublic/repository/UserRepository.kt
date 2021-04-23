b583031451e18d39de020c470e31bb4802e1db7e
Anthony Harvey <anthony21harvey@gmail.com>
2021-04-23 16:05:44 +0300

    Retrieving current user to observe for conditional navigation

--- app/src/main/java/fi/oamk/cottagerepublic/repository/UserRepository.kt
+++ app/src/main/java/fi/oamk/cottagerepublic/repository/UserRepository.kt
@@ -1,15 +1,17 @@
 package fi.oamk.cottagerepublic.repository
 
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 
 class UserRepository() {
-    private val databaseReference: DatabaseReference =Firebase.database.reference
+    private val databaseReference: DatabaseReference = Firebase.database.reference
     private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
-    private  var userid  = firebaseAuth.currentUser
+    private var userid  = firebaseAuth.currentUser
 
 
     fun createUser(email:String){
@@ -22,4 +24,10 @@ class UserRepository() {
 
         throw Exception("User isn't logged in")
     }
+
+    // Using MutableLiveData to notify AccountScreenFragment when current user has changed
+    fun getCurrentUser(): MutableLiveData<FirebaseUser> {
+        return MutableLiveData(firebaseAuth.currentUser)
+    }
+
 }

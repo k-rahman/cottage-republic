6277eb393fd65f637493ecab85ff45b7877757ae
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-04-27 02:28:47 +0300

    Refactor user settings fragment and layout

--- app/src/main/java/fi/oamk/cottagerepublic/ui/account/AccountUserSettingsScreenViewModel.kt
+++ app/src/main/java/fi/oamk/cottagerepublic/ui/account/AccountUserSettingsScreenViewModel.kt
@@ -1,46 +1,73 @@
 package fi.oamk.cottagerepublic.ui.account
 
-import android.util.Log
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import fi.oamk.cottagerepublic.repository.AuthRepository
 import fi.oamk.cottagerepublic.repository.UserRepository
 
-class AccountUserSettingsScreenViewModel : ViewModel() {
+class AccountUserSettingsScreenViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val userDataSource = UserRepository.getInstance(
-            Firebase.database.getReference("users"))
+    private val userDataSource = UserRepository.getInstance(Firebase.database.getReference("users"))
+    private val authDataSource = AuthRepository.getInstance(FirebaseAuth.getInstance())
+    private val userId = authDataSource.getCurrentUserId()
 
     var loading = MutableLiveData<Boolean>()
     var saveStatus = MutableLiveData<Boolean>()
 
-    val email = MutableLiveData("email")
-    val fname = MutableLiveData("fname")
-    val lname = MutableLiveData("lname")
-    val phone = MutableLiveData("phone")
+    val email = MutableLiveData("")
+    val fname = MutableLiveData("")
+    val lname = MutableLiveData("")
+    val phone = MutableLiveData("")
+
+    var loginFragment: Boolean? = null
+
     init {
         // will return true if get data and false if something goes wrong
-       if (!userDataSource.getCurrentUserData(email, fname, lname, phone)){
-           loading.value =false
-       }
+        userDataSource.getCurrentUserData(userId, email, fname, lname, phone)
     }
 
-    fun onSaveClick (){
-        Log.i("saveClick", "works")
-        saveStatus.value = (userDataSource.updateUserData(fname.value.toString(), lname.value.toString(), phone.value.toString()))
+    fun onSaveClick() {
+        if (fname.value.toString().isBlank() || lname.value.toString().isBlank() || phone.value.toString().isBlank()) {
+            showEmptyFieldsError()
+
+        } else {
+            userDataSource.updateUserData(
+                userId,
+                fname.value.toString(),
+                lname.value.toString(),
+                phone.value.toString()
+            )
+            if (loginFragment != null)
+                _navigateToLogin.value = true
+            else
+                _navigateToProfile.value = true
+        }
     }
+
+    private fun showEmptyFieldsError() {
+        Toast.makeText(getApplication(), "All fields are required", Toast.LENGTH_SHORT).show()
+    }
+
+    private val _navigateToLogin = MutableLiveData<Boolean>()
+    val navigateToLogin: LiveData<Boolean>
+        get() = _navigateToLogin
+
     private val _navigateToProfile = MutableLiveData<Boolean>()
-    val navigateToProfile:LiveData<Boolean>
-    get()= _navigateToProfile
-    fun onCancelClick (){
-        Log.i("CancelClick", "works")
-            _navigateToProfile.value = true
+    val navigateToProfile: LiveData<Boolean>
+        get() = _navigateToProfile
+
+    fun onLoginNavigated() {
+       _navigateToLogin.value = false
+        loginFragment = null
     }
-    fun onProfileNavigated(){
+
+    fun onProfileNavigated() {
         _navigateToProfile.value = false
     }
 
